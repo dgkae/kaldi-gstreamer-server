31f3be611306bb1b13bc4ce283194379fb5e1e66
dgkae <dgkae.cbf@gmail.com>
2016-08-09 17:26:02 +0800

    save to wav

--- dgkserver/master_server.py
+++ dgkserver/master_server.py
@@ -26,6 +26,7 @@ import tornado.gen
 import tornado.concurrent
 import settings
 import common
+import wave
 
 
 class Application(tornado.web.Application):
@@ -40,7 +41,7 @@ class Application(tornado.web.Application):
 
         handlers = [
             (r"/", MainHandler),
-            #(r"/client/ws/speech", DecoderSocketHandler),
+            (r"/client/ws/speech", DecoderSocketHandler),
             #(r"/client/ws/status", StatusSocketHandler),
             #(r"/client/dynamic/reference", ReferenceHandler),
             #(r"/client/dynamic/recognize", HttpChunkedRecognizeHandler),
@@ -272,12 +273,13 @@ class WorkerSocketHandler(tornado.websocket.WebSocketHandler):
     def set_client_socket(self, client_socket):
         self.client_socket = client_socket
 
-
+'''
 class DecoderSocketHandler(tornado.websocket.WebSocketHandler):
     # needed for Tornado 4.0
     def check_origin(self, origin):
         return True
 
+    '''
     def send_event(self, event):
         event["id"] = self.id
         event_str = str(event)
@@ -285,52 +287,31 @@ class DecoderSocketHandler(tornado.websocket.WebSocketHandler):
             event_str = event_str[:97] + "..."
         logging.info("%s: Sending event %s to client" % (self.id, event_str))
         self.write_message(json.dumps(event))
+    '''
 
     def open(self):
         self.id = str(uuid.uuid4())
         logging.info("%s: OPEN" % (self.id))
         logging.info("%s: Request arguments: %s" % (self.id, " ".join(["%s=\"%s\"" % (a, self.get_argument(a)) for a in self.request.arguments])))
-        self.user_id = self.get_argument("user-id", "none", True)
-        self.content_id = self.get_argument("content-id", "none", True)
-        self.worker = None
-        try:
-            self.worker = self.application.available_workers.pop()
-            self.application.send_status_update()
-            logging.info("%s: Using worker %s" % (self.id, self.__str__()))
-            self.worker.set_client_socket(self)
-
-            content_type = self.get_argument("content-type", None, True)
-            if content_type:
-                logging.info("%s: Using content type: %s" % (self.id, content_type))
-
-            self.worker.write_message(json.dumps(dict(id=self.id, content_type=content_type, user_id=self.user_id, content_id=self.content_id)))
-        except KeyError:
-            logging.warn("%s: No worker available for client request" % self.id)
-            event = dict(status=common.STATUS_NOT_AVAILABLE, message="No decoder available, try again later")
-            self.send_event(event)
-            self.close()
+        self.wav = wave.open(self.id + '.wav', 'wb')
+        self.wav.setparams((1,2,16000,0,'NONE','not compressed'))
+  
 
     def on_connection_close(self):
         logging.info("%s: Handling on_connection_close()" % self.id)
-        self.application.num_requests_processed += 1
-        self.application.send_status_update()
-        if self.worker:
-            try:
-                self.worker.set_client_socket(None)
-                logging.info("%s: Closing worker connection" % self.id)
-                self.worker.close()
-            except:
-                pass
 
     def on_message(self, message):
-        assert self.worker is not None
+
         logging.info("%s: Forwarding client message (%s) of length %d to worker" % (self.id, type(message), len(message)))
         if isinstance(message, unicode):
-            self.worker.write_message(message, binary=False)
+            # self.worker.write_message(message, binary=False)
+            pass
         else:
-            self.worker.write_message(message, binary=True)
+            # self.worker.write_message(message, binary=True)
+            self.wav.writeframes(message)
 
-'''
+
+
 def main():
     logging.basicConfig(level=logging.DEBUG, format="%(levelname)8s %(asctime)s %(message)s ")
     logging.debug('Starting up server')
